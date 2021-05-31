e0546e89887a00b0a4bb71b17f83d20a06378b5d
noure-d-zobiri <noure-d-zobiri@noures-mbp>
2021-05-31 10:03:17 -0400

    Testing

---
+++ NoureDalya_Juin/src/InterfaceBinding.java
@@ -0,0 +1,14 @@
+/*
+*Author: noure-d-zobiri
+*Date: May 31, 2021
+*Description:
+*/
+
+public class InterfaceBinding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
